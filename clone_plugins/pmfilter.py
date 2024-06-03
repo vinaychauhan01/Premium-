f68f5331897e6f9beca22940cb99c820a14fc8e8
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2024-06-03 19:28:21 +0530

    Update pmfilter.py

--- clone_plugins/pmfilter.py
+++ clone_plugins/pmfilter.py
@@ -15,6 +15,7 @@ from database.connections_mdb import active_connection, all_connections, delete_
     make_inactive
 from info import *
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
+from plugins.clone import clonedme
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from utils import get_size, is_req_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings, get_shortlink, get_tutorial, send_all, get_cap
